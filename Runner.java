8111e7a27ae57371719ce0b1e6955f39f2920076
chenevee <149628551+chenevee@users.noreply.github.com>
2026-05-15 19:07:55 -0700

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,1 +1,20 @@
+public class Runner{
+    private String name;
+    private String sponsorship;
+    private double result;
+    private double wind;
+    private int heat;
+    private int LN;
+    public Runner(String runner, String sponsor, double time, double windSpeed, int Temp, int POS){
+        name = runner;
+        sponsorship = sponsor;
+        result = time;
+        wind = windSpeed;
+        heat = Temp;
+        LN = POS;
+    }
+    public String toString(){
+        return name + " who is sponsored by " + sponsorship + " ended up with a time of " + result + ", the  conditions for him were a wind speed of" + wind + ", a temp of " + heat + ", and a LN of " + LN;
+    }
+}
 
